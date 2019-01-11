8d1e9286995b90948c8d2cbd437c891a3b1a217a
bondarenkojek <bondarenkojek@gmail.com>
2019-01-11 17:33:36 +0200

    tmp

--- src/main/java/ua/bondarenkojek/homework/jpa/Main.java
+++ src/main/java/ua/bondarenkojek/homework/jpa/Main.java
@@ -1,17 +1,20 @@
 package ua.bondarenkojek.homework.jpa;
 
 import ua.bondarenkojek.homework.jpa.dao.impl.PatientDaoImpl;
+import ua.bondarenkojek.homework.jpa.model.Patient;
 import ua.bondarenkojek.homework.jpa.model.test.BloodTest;
 import ua.bondarenkojek.homework.jpa.model.test.Test;
 import ua.bondarenkojek.homework.jpa.service.PatientService;
 import ua.bondarenkojek.homework.jpa.service.impl.PatientServiceImpl;
 import ua.bondarenkojek.homework.jpa.util.HibernateUtil;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        PatientService ps = new PatientServiceImpl(
-                new PatientDaoImpl(HibernateUtil.getEntityManager()));
-
+//        PatientService ps = new PatientServiceImpl(
+//                new PatientDaoImpl(HibernateUtil.getEntityManager()));
+//
 //        Patient patient = Patient
 //                .builder()
 //                .name("Jek")
